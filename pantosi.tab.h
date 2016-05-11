3a587651c5094105d04b9709e9f2eb27aaf7d311
KaiqueDamato <kaique_pantosi@hotmail.com>
2016-05-10 23:48:48 -0300

    Incluindo arquivos auxiliares no git conforme especificação do projeto

---
+++ pantosi.tab.h
@@ -0,0 +1,94 @@
+/* A Bison parser, made by GNU Bison 3.0.2.  */
+
+/* Bison interface for Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989-1990, 2000-2013 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+#ifndef YY_YY_PANTOSI_TAB_H_INCLUDED
+# define YY_YY_PANTOSI_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token type.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    LS = 258,
+    PS = 259,
+    KILL = 260,
+    MKDIR = 261,
+    RMDIR = 262,
+    CD = 263,
+    TOUCH = 264,
+    IFCONFIG = 265,
+    START = 266,
+    QUIT = 267,
+    N = 268,
+    STRING = 269,
+    PLUS = 270,
+    MINUS = 271,
+    TIMES = 272,
+    DIVIDED_BY = 273,
+    AP = 274,
+    FP = 275,
+    INT = 276,
+    FLOAT = 277
+  };
+#endif
+
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef union YYSTYPE YYSTYPE;
+union YYSTYPE
+{
+#line 27 "pantosi.y" /* yacc.c:1909  */
+
+	int i;
+	float f;
+	char c;
+	char* text;
+
+#line 84 "pantosi.tab.h" /* yacc.c:1909  */
+};
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+extern YYSTYPE yylval;
+
+int yyparse (void);
+
+#endif /* !YY_YY_PANTOSI_TAB_H_INCLUDED  */
